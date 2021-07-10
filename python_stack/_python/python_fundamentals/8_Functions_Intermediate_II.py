d61348cc63f7e80938ca99fd2ebec3999962cf6d
Meena Hira <meena.hira@gmail.com>
2021-07-10 18:19:05 -0500

    Iteration through Dictionary with Lists or List with dictionaries

--- python_stack/_python/python_fundamentals/8_Functions_Intermediate_II.py
+++ python_stack/_python/python_fundamentals/8_Functions_Intermediate_II.py
@@ -1,38 +1,38 @@
-####--- UPDATE VALUES IN DICTIONARIES AND LISTS---###
+# 1) ------UPDATE VALUES IN DICTIONARIES AND LISTS----###
 
-# 1) LIST OF LISTs--Change the value 10 in x to 15. Once you're done, x should now be [ [5,2,3], [15,8,9] ].
+# 1. LIST OF LISTs--Change the value 10 in x to 15. Once you're done, x should now be [ [5,2,3], [15,8,9] ].
 x = [ [5,2,3], [10,8,9] ] 
 x[1][0] = 15 # check- print index 0 of index 1 which is 10, assign to 15
-# print(x)
+print(x)
 #output- [[5, 2, 3], [15, 8, 9]]
 
-# 2) LIST OF DICTIONARIES--Change the last_name of the first student from 'Jordan' to 'Bryant'
+# 2. LIST OF DICTIONARIES--Change the last_name of the first student from 'Jordan' to 'Bryant'
 students = [
     {'first_name':  'Michael', 'last_name' : 'Jordan'},
     {'first_name' : 'John', 'last_name' : 'Rosales'}
 ]
 students[0]['last_name'] ='Bryan' # 0 index last_name access
-#print(students)
+print(students)
 #Output- [{'first_name': 'Michael', 'last_name': 'Bryan'}, {'first_name': 'John', 'last_name': 'Rosales'}]
     
-# 3) DICTIONARY OF LISTS-- In the sports_directory, change 'Messi' to 'Andres'.  
+# 3. DICTIONARY OF LISTS-- In the sports_directory, change 'Messi' to 'Andres'.  
 sports_directory = {
     'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
     'soccer' : ['Messi', 'Ronaldo', 'Rooney']
 }
 sports_directory['soccer'][0]= 'Andres' # In soccer key, index 0 of list access and change to Andres
-# print(sports_directory)
+print(sports_directory)
 #output- {'basketball': ['Kobe', 'Jordan', 'James', 'Curry'], 'soccer': ['Andres', 'Ronaldo', 'Rooney']}
 
-# 4) LIST OF DICTIONARIES--Change the value 20 in z to 30
+# 4. LIST OF DICTIONARIES--Change the value 20 in z to 30
 z = [ {'x': 10, 'y': 20} ]
 z[0]['y']= 30 # position 0 in list, then key y access, change to 30
-#print(z)
+print(z)
 # output-{'x': 10, 'y': 30}]
 
 
-####---ITERATE THROUGH LIST OF DICTIONARIES--##  
-#5) Create a function iterateDictionary(some_list) that, given a list of dictionaries, the function loops through each dictionary in the list and prints each key and the associated value.
+#2)------ITERATE THROUGH LIST OF DICTIONARIES----##  
+# Create a function iterateDictionary(some_list) that, given a list of dictionaries, the function loops through each dictionary in the list and prints each key and the associated value.
 # should output: (it's okay if each key-value pair ends up on 2 separate lines;
 # bonus to get them to appear exactly as below!)
 
@@ -77,41 +77,80 @@ iterateDictionary(students)
 # first_name - KB
 # last_name - Tonel
 
-3) Get Values From a List of Dictionaries
+# 3) -----GET VALUES FROM A LIST OF DICTIONARIES------####
 # Create a function iterateDictionary2(key_name, some_list) that, given a list of dictionaries and a key name, the function prints the value stored in that key for each dictionary. For example, iterateDictionary2('first_name', students) should output:
-Michael
-John
-Mark
-KB
+# Michael
+# John
+# Mark
+# KB
 # And iterateDictionary2('last_name', students) should output:
-Jordan
-Rosales
-Guillen
-Tonel
+# Jordan
+# Rosales
+# Guillen
+# Tonel
 
-# 4)Iterate Through a Dictionary with List Values
-# Create a function printInfo(some_dict) that given a dictionary whose values are all lists, prints the name of each key along with the size of its list, and then prints the associated values within each key's list.
-dojo = {
-'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
-'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
-}
-printInfo(dojo)
-# output:
-7 LOCATIONS
-San Jose
-Seattle
-Dallas
-Chicago
-Tulsa
-DC
-Burbank
+def iterateDictionary2(key_name, some_list):
+    for idx in range(len(some_list)):
+        print(some_list[idx][key_name])
+students = [                       # argument student passed through parameter
+        {'first_name':  'Michael', 'last_name' : 'Jordan'},
+        {'first_name' : 'John', 'last_name' : 'Rosales'},
+        {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+        {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    ]
+iterateDictionary2('first_name', students)
+iterateDictionary2('last_name', students)
+# OUTPUTS- Michael    Jordan
+#          John       Rosales
+#          Mark       Guillen
+#          KB         Tonel
+MLB_team = [
+    {'State':'Colorado', 'Team' : 'Rockies'},
+    {'State':'Boston', 'Team': 'Red Sox'},
+    {'State':'Minnesota', 'Team' : 'Twins'},
+    {'State':'Milwaukee', 'Team' : 'Brewers'},
+    {'State':'Seattle' , 'Team' : 'Mariners'}
+] 
+iterateDictionary2('Team', MLB_team)  
+#OUTPUT- Rockies
+#       Red Sox
+#       Twins
+#       Brewers
+#       Mariners
     
-8 INSTRUCTORS
-Michael
-Amy
-Eduardo
-Josh
-Graham
-Patrick
-Minh
-Devon
+
+# 4)ITERATE THROUGH A DICTIONARY WITH LIST VALUES
+# Create a function printInfo(some_dict) that given a dictionary whose values are all lists, prints the name of each key along with the size of its list, and then prints the associated values within each key's list.
+def printInfo(some_dict):
+    for key in some_dict: #accessing key in dictionary
+        print(len(some_dict[key]), key.upper()) # length of key and .upper()to covert to uppercase
+        for val in some_dict[key]: # inner for loop to access values further in key
+            print(val)
+        
+dojo = {
+    'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
+    'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+}
+
+printInfo(dojo)
+
+
+# # output:
+# 7 LOCATIONS
+# San Jose
+# Seattle
+# Dallas
+# Chicago
+# Tulsa
+# DC
+# Burbank
+    
+# 8 INSTRUCTORS
+# Michael
+# Amy
+# Eduardo
+# Josh
+# Graham
+# Patrick
+# Minh
+# Devon
