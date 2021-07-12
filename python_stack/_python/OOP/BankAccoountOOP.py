7ab6956be5c321c13194eaabe94eff30c51521c5
Meena Hira <meena.hira@gmail.com>
2021-07-11 23:49:59 -0500

    --

--- python_stack/_python/OOP/BankAccoountOOP.py
+++ python_stack/_python/OOP/BankAccoountOOP.py
@@ -1,9 +1,9 @@
 class BankAccount:
-    def __init__(self, username, int_rate, balance=0):
+    def __init__(self, username, int_rate, balance):
         #adding default values for parameters
         self.name = username 
         self.rate = (int_rate * .01)
-        self.balance = balance
+        self.balance = 0
 
     def deposit(self, amount):# increases the account balance by the given amount
         self.balance += amount 
@@ -13,7 +13,7 @@ class BankAccount:
         amount_after_withdraw = self.balance - amount
         # if there is not enough money, print a message "Insufficient funds: Charging a $5 fee" and deduct $5
         if amount_after_withdraw < 0:
-            print("Insufficient funds: Charging a $5 fee")
+            print(f"User: {self.name}-Insufficient funds: Charging a $5 fee")
             self.balance -= 5
         else:
             self.balance -= amount
@@ -41,6 +41,6 @@ emily.deposit(1000).deposit(200).deposit(50).withdraw(500).yield_interest().disp
 tanya.deposit(220).deposit(50).withdraw(100).withdraw(100).withdraw(60).withdraw(50).yield_interest().display_account_info()
 
 #OUTPUT----
-# User: Emily, Balance: $858.5
-# Insufficient funds: Charging a $5 fee
-# User: Tanya, Balance: $15.18
+# User: Emily, Balance: $757.5
+# User: Tanya-Insufficient funds: Charging a $5 fee
+# User: Tanya, Balance: $5.06
