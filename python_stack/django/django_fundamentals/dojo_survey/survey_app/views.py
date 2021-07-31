4872ca1606986b80fb656a7345ea7c12a8fe0d7c
Meena Hira <meena.hira@gmail.com>
2021-07-31 14:47:00 -0500

    survey form- Django app

--- python_stack/django/django_fundamentals/dojo_survey/survey_app/views.py
+++ python_stack/django/django_fundamentals/dojo_survey/survey_app/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, HttpResponse
 
 # Create your views here.
+def index(request):
+    # return HttpResponse("working")
+    return render(request, "form.html")
+
+def process(request):
+    # return render(request, "result.html")
+    if request.method == "POST":
+        print(request.POST)
+        context = {
+            'name': request.POST['name'],
+            # 'gen': request.POST['Gender'],
+            'lang':request.POST['languages'],
+            'loc':request.POST['location'],
+    
+        }
+        return render(request, "result.html", context)
+    return render(request, "result.html") 
+
+
+def new(request): #have to figure out how to redirect to index through process submit button without this method
+    # if request.method == "GET":
+    #     print(request.POST)
+        return redirect('/')
+    
